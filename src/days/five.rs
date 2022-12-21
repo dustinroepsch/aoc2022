9b97852999f7a5b93093939a2ccf57b4b4c42bdd
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2022-12-20 21:56:48 -0800

    clippy

--- src/days/five.rs
+++ src/days/five.rs
@@ -8,7 +8,7 @@ use super::Day;
 pub const DAY_FIVE: Day = Day { part_one, part_two };
 
 #[cfg(windows)]
-const LINE_ENDING: &'static str = "\r\n";
+const LINE_ENDING: &str = "\r\n";
 #[cfg(not(windows))]
 const LINE_ENDING: &'static str = "\n";
 

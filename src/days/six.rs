8e467b92fbf288ef16aebed93df1da73901c71dd
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2022-12-20 19:07:55 -0800

    day 6 --all

--- src/days/six.rs
+++ src/days/six.rs
@@ -1,11 +1,31 @@
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+
 use super::Day;
 
 pub const DAY_SIX: Day = Day { part_one, part_two };
 
-fn part_one(_input: &str) -> String {
-    todo!()
+fn part_one(input: &str) -> String {
+    let mut buf = input.chars().take(4).collect::<VecDeque<_>>();
+    let mut total_taken = 4;
+    let mut chars = input.chars().skip(4);
+    while !buf.iter().all_unique() {
+        buf.pop_front();
+        buf.push_back(chars.next().unwrap());
+        total_taken += 1; 
+    }
+    total_taken.to_string()
 }
 
-fn part_two(_input: &str) -> String {
-    todo!()
+fn part_two(input: &str) -> String {
+    let mut buf = input.chars().take(14).collect::<VecDeque<_>>();
+    let mut total_taken = 14;
+    let mut chars = input.chars().skip(14);
+    while !buf.iter().all_unique() {
+        buf.pop_front();
+        buf.push_back(chars.next().unwrap());
+        total_taken += 1; 
+    }
+    total_taken.to_string()
 }
